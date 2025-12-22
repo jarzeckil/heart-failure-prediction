f76125cb191cbf9df72bd21089607cf16741d67e
jarzeckil <jarzeckil@gmail.com>
2025-12-22 16:46:39 +0100

    fix: saving raw model instead of MLFlow wrapper

--- src/heart_failure_prediction/export_model.py
+++ src/heart_failure_prediction/export_model.py
@@ -1,6 +1,8 @@
 import joblib
 import mlflow
 
-model = mlflow.pyfunc.load_model(model_uri='models:/heart_failure_prediction/latest')
+model = mlflow.pyfunc.load_model(
+    model_uri='models:/heart_failure_prediction/latest'
+).get_raw_model()
 
 joblib.dump(model, 'models/model.joblib')
