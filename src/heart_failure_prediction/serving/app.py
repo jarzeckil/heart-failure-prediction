5356fd58a82cfffe3cf5b372ff91aae567626c3a
jarzeckil <jarzeckil@gmail.com>
2025-12-22 17:06:46 +0100

    feature: created api for serving model

---
+++ src/heart_failure_prediction/serving/app.py
@@ -0,0 +1,59 @@
+from contextlib import asynccontextmanager
+import logging
+import os.path
+
+from fastapi import FastAPI, HTTPException
+import joblib
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+from heart_failure_prediction.config import MODEL_DIR
+from heart_failure_prediction.serving.schemas import HeartDiseaseRecord
+
+logger = logging.getLogger(__name__)
+
+models = {}
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    model_folder = MODEL_DIR
+    model_name = 'model.joblib'
+    model_path = os.path.join(model_folder, model_name)
+    try:
+        model = joblib.load(model_path)
+        models['model'] = model
+        logger.info('Model loaded successfully')
+    except FileNotFoundError:
+        logger.error(f"Couldn't read model from path {model_path}")
+
+    yield
+
+    models.clear()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post('/predict')
+async def predict(record: HeartDiseaseRecord):
+    model: Pipeline = models.get('model')
+
+    if model is None:
+        logger.error("Model wasn't loaded")
+        raise HTTPException(status_code=503, detail='Service unavailable')
+
+    try:
+        data = pd.DataFrame.from_records([record.model_dump()])
+        pred = model.predict(data)
+        pred_proba = model.predict_proba(data)
+
+        return {
+            'HeartDisease': int(pred[0]),
+            'Probability-positive': float(pred_proba[0][1]),
+            'Probability-negative': float(pred_proba[0][0]),
+        }
+
+    except Exception as e:
+        logger.error(f'Error during prediction phase: {e}')
+        raise HTTPException(status_code=500) from e
